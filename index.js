af4f551ec5b0cb1a6d7eca15e1dd111b632d1c9e
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-05-28 22:18:20 +0200

    step 1

---
+++ index.js
@@ -0,0 +1,14 @@
+// setTimeout(() => {
+//   console.log("timeout 3s");
+// }, 2000);
+
+// setImmediate(function () {
+//   console.log("setImmediate callback");
+// });
+
+// process.nextTick(function () {
+//   console.log("NextTick callback");
+// });
+
+// // NextTick callback
+// // setImmediate callback
